6161caed4c293f835226e919d4fe8981beb8dbd6
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-25 22:26:33 -0500

    added more rules and driver code -- tested a few paths, working as expected tho with some instability

--- bot.py
+++ bot.py
@@ -4,10 +4,29 @@ from nltk.chat.util import Chat, reflections
 
 ############------------ GLOBAL VARIABLES ------------############
 rules = [
-    "my name is (.*)",
+    r"hi|Hi|hey|Hey|hello|Hello",
+    ["yo -- what's going on?", "well, hello there!",]
+], [
+    r"my name is (.*)",
     ["hi %1, what's up?"]
-],
-[
+], [
+    "yes",
+    ["... and what else is new, %1?"]
+], [
     "what's your name?",
     ["call me bot ;)"]
 ]
+
+############------------ FUNCTION(S) ------------############
+def chat_with_the_bot():
+    print("this is bot -- you built me, remember?")
+
+
+chat_with_the_bot()
+
+
+############------------ GLOBAL VARIABLES ------------############
+if __name__ == "__main__":
+    chat = Chat(rules, reflections)
+    chat.converse()
+    
