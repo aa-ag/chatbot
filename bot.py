a9ff7020f41c6ca862303756b42f5f8ce13bec9e
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-25 22:36:02 -0500

    fixed [] in rules -- working stably, missing full rules for a conversation

--- bot.py
+++ bot.py
@@ -4,29 +4,35 @@ from nltk.chat.util import Chat, reflections
 
 ############------------ GLOBAL VARIABLES ------------############
 rules = [
-    r"hi|Hi|hey|Hey|hello|Hello",
-    ["yo -- what's going on?", "well, hello there!",]
-], [
-    r"my name is (.*)",
-    ["hi %1, what's up?"]
-], [
-    "yes",
-    ["... and what else is new, %1?"]
-], [
-    "what's your name?",
-    ["call me bot ;)"]
+    [
+        r"hi|Hi|hey|Hey|hello|Hello",
+        ["yo -- what's going on?", "well, hello there!",]
+    ], 
+    [
+        r"my name is (.*)",
+        ["hi %1, what's up?"]
+    ], 
+    [
+        r"what's your name ?",
+        ["call me bot ;)"]
+    ], 
+    [
+        r"quit|Quit|QUIT|exit|Exit|bye|Bye|BYE",
+        ["buh-bye"]
+    ],
 ]
+
 
 ############------------ FUNCTION(S) ------------############
 def chat_with_the_bot():
     print("this is bot -- you built me, remember?")
 
 
-chat_with_the_bot()
-
-
 ############------------ GLOBAL VARIABLES ------------############
+chat = Chat(rules, reflections)
+chat.converse()
 if __name__ == "__main__":
-    chat = Chat(rules, reflections)
-    chat.converse()
+    chat_with_the_bot()
+    
+    
     
