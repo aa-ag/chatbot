873fcd62d704a3e0daf71a4a30c0f988e89c99c2
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-25 22:12:24 -0500

    printed reflections to see what comes with that object from the module

--- bot.py
+++ bot.py
@@ -1,7 +1,27 @@
 ############------------ IMPORT(S) ------------############
-import nltk
+import nltk # https://www.nltk.org/
 from nltk.chat.util import Chat, reflections
 
 ############------------ CODE ------------############
 print(Chat)
 # <class 'nltk.chat.util.Chat'>
+
+print(reflections)
+'''
+{'i am': 'you are', 
+'i was': 'you were', 
+'i': 'you', 
+"i'm": 'you are', 
+"i'd": 'you would', 
+"i've": 'you have', 
+"i'll": 'you will', 
+'my': 'your', 
+'you are': 'I am', 
+'you were': 'I was',
+ "you've": 'I have', 
+ "you'll": 'I will', 
+ 'your': 'my', 
+ 'yours': 'mine', 
+ 'you': 'me', 
+ 'me': 'you'}
+'''
