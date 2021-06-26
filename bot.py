8cc2e7d43587216fa6725411d3da15a6ddcb0306
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-25 22:07:31 -0500

    added logic to print Chat class to verify imports are working

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 ############------------ IMPORT(S) ------------############
-
+import nltk
+from nltk.chat.util import Chat, reflections
 
 ############------------ CODE ------------############
+print(Chat)
+# <class 'nltk.chat.util.Chat'>
