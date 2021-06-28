347607bc560f63f755c527dd81e0584975a72df6
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-27 19:22:43 -0500

    updated README to reflect channels install

--- django_app/botgui/asgi.py
+++ django_app/botgui/asgi.py
@@ -1,8 +1,8 @@
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'botgui.settings')
 
